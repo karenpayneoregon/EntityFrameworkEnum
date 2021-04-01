2b84388d79d95121459537318642be92de5303a3
Karen Payne <Karen.1.Payne@oregon.gov>
2021-04-01 10:31:32 -0700

    Added new table to convert to enum

---
+++ EnumGeneratorLibrary/LanguageCodes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace EnumGeneratorLibrary
+{
+    /// <summary>
+    /// LanguageCodes auto generated enumeration by Karen Payne
+    /// </summary>
+    [GeneratedCode("TextTemplatingFileGenerator", "10")]
+    public enum LanguageCodes
+    {
+      
+		/// <summary>
+        /// English 
+        /// </summary>
+        English = 1,
+
+      
+		/// <summary>
+        /// Spanish 
+        /// </summary>
+        Spanish = 2
+  }
+}
